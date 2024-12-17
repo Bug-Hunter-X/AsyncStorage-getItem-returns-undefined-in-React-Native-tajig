b28ff6d03750c51ed0b07c402f035713c7b93b6e
BugHunter-X <askzenai@gmail.com>
2024-12-17 17:46:07 +0100

    Add AsyncStorageBug.js file

---
+++ AsyncStorageBug.js
@@ -0,0 +1,1 @@
+This error occurs when using AsyncStorage in React Native and you try to access the stored data before it has been fully retrieved.  It often manifests as undefined or null values even though the data exists in AsyncStorage.  The problem stems from the asynchronous nature of AsyncStorage's getItem method.
